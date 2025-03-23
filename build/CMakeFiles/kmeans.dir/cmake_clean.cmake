788eb90d26f1af867d37b031eeb96982758cef30
thiago.vicente@ua.pt <thiago.vicente@ua.pt>
2025-03-23 16:02:12 +0000

    nothing

---
+++ build/CMakeFiles/kmeans.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kmeans.dir/link.d"
+  "CMakeFiles/kmeans.dir/main.cpp.o"
+  "CMakeFiles/kmeans.dir/main.cpp.o.d"
+  "CMakeFiles/kmeans.dir/src/Kmeans.cpp.o"
+  "CMakeFiles/kmeans.dir/src/Kmeans.cpp.o.d"
+  "CMakeFiles/kmeans.dir/src/Point.cpp.o"
+  "CMakeFiles/kmeans.dir/src/Point.cpp.o.d"
+  "CMakeFiles/kmeans.dir/src/Utils.cpp.o"
+  "CMakeFiles/kmeans.dir/src/Utils.cpp.o.d"
+  "kmeans"
+  "kmeans.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kmeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
